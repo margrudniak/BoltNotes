93cd8ff1a9d458645ce3cd4c1d45226beebef289
Marcin Grudniak <maverickmonkey69@gmail.com>
2022-01-23 15:35:06 +0100

    feat: adjust HomeScreen

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,8 +1,38 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {useTheme} from '@react-navigation/native';
+import {StatusBar, Text, View} from 'react-native';
+import {SafeAreaView, useSafeAreaInsets} from 'react-native-safe-area-context';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {DateBar, Header, Modal} from '../../components';
 
 export interface HomeScreenProps {}
 
 export const HomeScreen = ({message}: HomeScreenProps) => {
-  return <View style={{flex: 1, backgroundColor: 'red'}}></View>;
+  const [modalVisible, setModalVisible] = useState(false);
+  const {dark, colors} = useTheme();
+  const {bottom, right} = useSafeAreaInsets();
+  useEffect(() => console.log(modalVisible), [modalVisible]);
+  return (
+    <SafeAreaView
+      edges={['top', 'left', 'right']}
+      style={{flex: 1, backgroundColor: colors.background}}>
+      <StatusBar barStyle={dark ? 'light-content' : 'dark-content'} />
+      <Header onPressNote={setModalVisible} />
+      <DateBar />
+      <Icon
+        size={60}
+        name="add-circle"
+        color={colors.primary}
+        onPress={() => console.log('clicked add')}
+        style={{
+          position: 'absolute',
+          bottom: 0,
+          right: 0,
+          marginBottom: bottom,
+          marginRight: right,
+        }}
+      />
+      <Modal {...{modalVisible, setModalVisible}} />
+    </SafeAreaView>
+  );
 };
