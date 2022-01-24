187829e4bde812b1da886f18f4869cb00774d9eb
Marcin Grudniak <maverickmonkey69@gmail.com>
2022-01-24 14:34:12 +0100

    feat: add styles to button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,26 +1,41 @@
 import {useTheme} from '@react-navigation/native';
 import React from 'react';
-import {GestureResponderEvent, Text, TouchableOpacity} from 'react-native';
+import {
+  GestureResponderEvent,
+  StyleProp,
+  Text,
+  TouchableOpacity,
+  ViewStyle,
+} from 'react-native';
 
 export interface ButtonProps {
+  style: StyleProp<ViewStyle> | StyleProp<ViewStyle>[];
   onPress: (event: GestureResponderEvent) => void;
   text: string;
   textColor?: string;
 }
 
-export const Button = ({onPress, text, textColor = 'black'}: ButtonProps) => {
+export const Button = ({
+  style,
+  onPress,
+  text,
+  textColor = 'black',
+}: ButtonProps) => {
   const {colors} = useTheme();
   return (
     <TouchableOpacity
       {...{onPress}}
-      style={{
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderColor: colors.primary,
-        borderWidth: 3,
-        borderRadius: 10,
-        backgroundColor: colors.primary,
-      }}>
+      style={[
+        {
+          justifyContent: 'center',
+          alignItems: 'center',
+          borderColor: colors.primary,
+          borderWidth: 3,
+          borderRadius: 10,
+          backgroundColor: colors.primary,
+        },
+        style,
+      ]}>
       <Text style={{color: textColor}}>{text}</Text>
     </TouchableOpacity>
   );
