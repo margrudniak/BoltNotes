21a3b4d5ea41c8df3d97632788003e0e63b3391a
Marcin Grudniak <maverickmonkey69@gmail.com>
2022-01-24 14:33:57 +0100

    feat: reconfig Modal button

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,5 +1,5 @@
+import React, {Dispatch, SetStateAction, useRef, useState} from 'react';
 import {useTheme} from '@react-navigation/native';
-import React, {Dispatch, SetStateAction} from 'react';
 import {
   Modal as NativeModal,
   Text,
@@ -8,6 +8,7 @@ import {
   View,
 } from 'react-native';
 import {Button} from '..';
+import {checkIfExceeded, getDimensions} from '../../utils';
 
 export interface ModalProps {
   modalVisible: boolean;
@@ -15,18 +16,30 @@ export interface ModalProps {
 }
 
 export const Modal = ({modalVisible, setModalVisible}: ModalProps) => {
-  const [text, onChangeText] = React.useState('');
+  const [text, setText] = useState('');
   const {dark} = useTheme();
+  const {width} = getDimensions('window');
+  const refInput = useRef<TextInput>(null);
+  const maxLength = 130;
+
+  const onDismissModal = () => {
+    setModalVisible(false);
+    setText('');
+  };
+
+  const changeColor = () =>
+    checkIfExceeded(text.length, maxLength) ? 'red' : 'black';
+
   return (
     <View>
       <NativeModal
         animationType="slide"
         transparent={true}
         visible={modalVisible}
-        onRequestClose={() => setModalVisible(false)}>
+        onRequestClose={onDismissModal}>
         <TouchableOpacity
           activeOpacity={1}
-          onPressOut={() => setModalVisible(false)}
+          onPressOut={onDismissModal}
           style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
           <View
             style={{
@@ -46,8 +59,34 @@ export const Modal = ({modalVisible, setModalVisible}: ModalProps) => {
               elevation: 12,
             }}>
             <Text>{'Nowa notatka'}</Text>
-            <TextInput onChangeText={onChangeText} value={text} />
-            <Button text="ZAPISZ" onPress={() => setModalVisible(false)} />
+            <TextInput
+              ref={refInput}
+              onLayout={() => refInput?.current?.focus()}
+              multiline
+              maxLength={maxLength}
+              onChangeText={setText}
+              value={text}
+              style={{
+                margin: 10,
+                padding: 10,
+                borderColor: changeColor(),
+                borderWidth: 2,
+                borderRadius: 10,
+                width: 0.8 * width,
+                height: 100,
+              }}
+            />
+            <Text
+              style={{
+                color: changeColor(),
+                alignSelf: 'flex-end',
+                padding: 5,
+              }}>{`${text.length} / ${maxLength}`}</Text>
+            <Button
+              text="ZAPISZ"
+              onPress={onDismissModal}
+              style={{height: 40}}
+            />
           </View>
         </TouchableOpacity>
       </NativeModal>
