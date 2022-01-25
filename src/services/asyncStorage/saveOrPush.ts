28b533026c0cd53cdf2d4d44fb408b4b7e498f92
Marcin Grudniak <maverickmonkey69@gmail.com>
2022-01-25 18:45:56 +0100

    feat; add saveOrPush sync with asyncStorage

---
+++ src/services/asyncStorage/saveOrPush.ts
@@ -0,0 +1,32 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {getFormatedDate} from '../../utils';
+
+export const saveOrPush = async (value: string) => {
+  try {
+    await AsyncStorage.getItem(
+      getFormatedDate(new Date()),
+      async (err, result) => {
+        try {
+          if (result !== null) {
+            console.log('Data Found', result);
+            const newIds = JSON.parse(result!).concat(`,${value}`);
+            AsyncStorage.setItem(
+              getFormatedDate(new Date()),
+              JSON.stringify(newIds),
+            );
+          } else {
+            console.log('Data Not Found');
+            await AsyncStorage.setItem(
+              getFormatedDate(new Date()),
+              JSON.stringify(value),
+            );
+          }
+        } catch (e) {
+          console.log('err', err);
+        }
+      },
+    );
+  } catch (e) {
+    console.log('error', e);
+  }
+};
