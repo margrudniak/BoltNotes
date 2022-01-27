44929518c3fab11e886565ffa9a69cbec878a8db
Marcin Grudniak <maverickmonkey69@gmail.com>
2022-01-27 16:57:05 +0100

    feat: adjust Home view for VoiceRecord

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,9 +1,9 @@
 import React, {useEffect, useState} from 'react';
 import {useTheme} from '@react-navigation/native';
-import {StatusBar} from 'react-native';
+import {StatusBar, View} from 'react-native';
 import {SafeAreaView, useSafeAreaInsets} from 'react-native-safe-area-context';
 import Icon from 'react-native-vector-icons/Ionicons';
-import {DateBar, Header, Item, Modal} from '../../components';
+import {DateBar, Header, Item, Modal, VoiceRecord} from '../../components';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {containsObject, objMap} from '../../utils';
 import {deleteItem, getAll} from '../../services/asyncStorage';
@@ -18,6 +18,7 @@ export const HomeScreen = ({}: HomeScreenProps) => {
   const [render, setRender] = useState(false);
   const [values, setValues] = useState<itemType[]>([]);
   const [modalVisible, setModalVisible] = useState(false);
+  const [voiceRecord, setVoiceRecord] = useState(false);
   const {dark, colors} = useTheme();
   const {bottom, right} = useSafeAreaInsets();
 
@@ -56,63 +57,45 @@ export const HomeScreen = ({}: HomeScreenProps) => {
   };
 
   return (
-    <SafeAreaView
-      edges={['top', 'left', 'right']}
-      style={{flex: 1, backgroundColor: colors.background}}>
-      <StatusBar barStyle={dark ? 'light-content' : 'dark-content'} />
-      <Header onPressNote={setModalVisible} />
-      {renderContent()}
-      <Icon
-        size={60}
-        name="add-circle"
-        color={colors.primary}
-        onPress={async () => {
-          let test = await AsyncStorage.getAllKeys();
-          console.log(test);
-        }}
-        style={{
-          position: 'absolute',
-          bottom: 0,
-          right: 0,
-          marginBottom: bottom,
-          marginRight: right,
-        }}
-      />
-      <Icon
-        size={60}
-        name="add-circle"
-        color={'green'}
-        onPress={async () => {
-          let test = await AsyncStorage.clear();
-          console.log(test);
-        }}
-        style={{
-          position: 'absolute',
-          bottom: 0,
-          right: 100,
-          marginBottom: bottom,
-          marginRight: right,
-        }}
-      />
-      <Icon
-        size={60}
-        name="add-circle"
-        color={'red'}
-        onPress={async () =>
-          await AsyncStorage.setItem(
-            '@MySuperStore:key',
-            JSON.stringify(['test1']),
-          )
-        }
-        style={{
-          position: 'absolute',
-          bottom: 0,
-          left: 0,
-          marginBottom: bottom,
-          marginRight: right,
-        }}
-      />
-      <Modal {...{modalVisible, setModalVisible, onSaveData}} />
-    </SafeAreaView>
+    <>
+      <SafeAreaView
+        edges={['top', 'left', 'right']}
+        style={{flex: 1, backgroundColor: colors.background}}>
+        <StatusBar barStyle={dark ? 'light-content' : 'dark-content'} />
+        <Header onPressNote={setModalVisible} />
+        {renderContent()}
+        <Icon
+          size={60}
+          name="add-circle"
+          color={'red'}
+          onPress={async () => {
+            let test = await AsyncStorage.clear();
+            console.log(test);
+          }}
+          style={{
+            position: 'absolute',
+            bottom: 0,
+            left: 0,
+            marginBottom: bottom,
+            marginRight: right,
+          }}
+        />
+        <Icon
+          size={60}
+          name="add-circle"
+          color={colors.primary}
+          onPress={() => setVoiceRecord(true)}
+          style={{
+            position: 'absolute',
+            bottom: 0,
+            right: 0,
+            marginBottom: bottom,
+            marginRight: right,
+          }}
+        />
+        <Modal {...{modalVisible, setModalVisible, onSaveData}} />
+      </SafeAreaView>
+      {voiceRecord && <VoiceRecord />}
+    </>
   );
 };
