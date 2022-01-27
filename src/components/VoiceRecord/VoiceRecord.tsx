fc956bbca939e50b0f5a0e37da74812cde20616c
Marcin Grudniak <maverickmonkey69@gmail.com>
2022-01-27 16:56:43 +0100

    feat: add VoiceRecord component

---
+++ src/components/VoiceRecord/VoiceRecord.tsx
@@ -0,0 +1,177 @@
+import React, {useEffect, useLayoutEffect, useRef, useState} from 'react';
+import {Animated, Easing, Text, View} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Voice, {
+  SpeechRecognizedEvent,
+  SpeechResultsEvent,
+  SpeechErrorEvent,
+  SpeechStartEvent,
+  SpeechEndEvent,
+  SpeechVolumeChangeEvent,
+} from '@react-native-voice/voice';
+
+export interface VoiceRecordProps {}
+
+export const VoiceRecord = ({}: VoiceRecordProps) => {
+  const [pitch, setPitch] = useState('');
+  const [recognized, setRecognized] = useState('');
+  const [error, setError] = useState('');
+  const [end, setEnd] = useState('');
+  const [started, setStarted] = useState('');
+  const [results, setResults] = useState([]);
+  const [partialResults, setPartialResults] = useState([]);
+  const spinValue = useRef(new Animated.Value(0)).current;
+  const AnimatedIcon = Animated.createAnimatedComponent(Icon);
+
+  useEffect(() => {
+    Animated.loop(
+      Animated.timing(spinValue, {
+        toValue: 1,
+        duration: 2000,
+        easing: Easing.linear,
+        useNativeDriver: true,
+      }),
+    ).start();
+
+    //Setting callbacks for the process status
+    Voice.onSpeechStart = onSpeechStart;
+    Voice.onSpeechRecognized = onSpeechRecognized;
+    Voice.onSpeechEnd = onSpeechEnd;
+    Voice.onSpeechError = onSpeechError;
+    Voice.onSpeechResults = onSpeechResults;
+    Voice.onSpeechPartialResults = onSpeechPartialResults;
+    Voice.onSpeechVolumeChanged = onSpeechVolumeChanged;
+    setTimeout(() => {
+      startRecognizing();
+    }, 0);
+
+    return () => {
+      //destroy the process after switching the screen
+      Voice.destroy().then(Voice.removeAllListeners);
+    };
+  }, []);
+
+  const onSpeechStart = (e: SpeechStartEvent) => {
+    //Invoked when .start() is called without error
+    console.log('onSpeechStart: ', e);
+    setStarted('√');
+  };
+
+  const onSpeechRecognized = (e: SpeechRecognizedEvent) => {
+    //Invoked when .start() is called without error
+    console.log('onSpeechStart: ', e);
+    setRecognized('√');
+  };
+
+  const onSpeechEnd = (e: SpeechEndEvent) => {
+    //Invoked when SpeechRecognizer stops recognition
+    console.log('onSpeechEnd: ', e);
+    setEnd('√');
+  };
+
+  const onSpeechError = (e: SpeechErrorEvent) => {
+    //Invoked when an error occurs.
+    console.log('onSpeechError: ', e);
+    setError(JSON.stringify(e.error));
+  };
+
+  const onSpeechResults = (e: SpeechResultsEvent) => {
+    //Invoked when SpeechRecognizer is finished recognizing
+    console.log('onSpeechResults: ', e);
+    setResults(e.value);
+  };
+
+  const onSpeechPartialResults = (e: SpeechResultsEvent) => {
+    //Invoked when any results are computed
+    console.log('onSpeechPartialResults: ', e);
+    setPartialResults(e.value);
+  };
+
+  const onSpeechVolumeChanged = (e: SpeechVolumeChangeEvent) => {
+    //Invoked when pitch that is recognized changed
+    console.log('onSpeechVolumeChanged: ', e);
+    setPitch(e.value);
+  };
+
+  const startRecognizing = async () => {
+    //Starts listening for speech for a specific locale
+    try {
+      console.log('startRecognizing');
+      await Voice.start('en-US');
+      setPitch('');
+      setError('');
+      setStarted('');
+      setResults([]);
+      setPartialResults([]);
+      setEnd('');
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const stopRecognizing = async () => {
+    //Stops listening for speech
+    try {
+      await Voice.stop();
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const cancelRecognizing = async () => {
+    //Cancels the speech recognition
+    try {
+      await Voice.cancel();
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const destroyRecognizer = async () => {
+    //Destroys the current SpeechRecognizer instance
+    try {
+      await Voice.destroy();
+      setPitch('');
+      setError('');
+      setStarted('');
+      setResults([]);
+      setPartialResults([]);
+      setEnd('');
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <View
+      style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(52, 52, 52, 0.5)',
+      }}>
+      {results.map((result, index) => {
+        return <Text key={`result-${index}`}>{result}</Text>;
+      })}
+      <AnimatedIcon
+        size={200}
+        name="microphone"
+        onPress={startRecognizing}
+        style={{
+          transform: [
+            {
+              rotate: spinValue.interpolate({
+                inputRange: [0, 1],
+                outputRange: ['0deg', '360deg'],
+              }),
+            },
+          ],
+        }}
+      />
+    </View>
+  );
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export * from './Header/Header';
 export * from './Item/Item';
 export * from './Modal/Modal';
 export * from './Button/Button';
+export * from './VoiceRecord/VoiceRecord';
