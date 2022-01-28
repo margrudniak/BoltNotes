8593a1acd993235744051acf15f250fae27cc948
Marcin Grudniak <maverickmonkey69@gmail.com>
2022-01-28 14:36:15 +0100

    fix: add flexibility on Item compoenent

--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -23,11 +23,20 @@ export const Item = ({title, onDelete}: ItemProps) => {
         flexDirection: 'row',
         backgroundColor: 'white',
         alignItems: 'center',
-        justifyContent: 'space-between',
+        justifyContent: 'center',
         padding: 10,
       }}>
-      <Text>{title}</Text>
-      <Icon size={30} name="delete" color={'black'} onPress={handleDelete} />
+      <Text style={{flex: 6}}>{title}</Text>
+      <Icon
+        size={30}
+        name="delete"
+        color={'black'}
+        onPress={handleDelete}
+        style={{
+          padding: 5,
+          paddingLeft: 10,
+        }}
+      />
     </View>
   );
 };
