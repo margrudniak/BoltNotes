18a33273b58af84f4f1b3d1c2552b406d01fbe56
Marcin Grudniak <maverickmonkey69@gmail.com>
2022-03-10 18:20:30 +0100

    feat: ignore all logs

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import {LogBox} from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {AppNavigator} from './src/navigation';
 
 const App = () => {
+  LogBox.ignoreAllLogs(); //Ignore all log notifications
   return (
     <SafeAreaProvider>
       <AppNavigator />
